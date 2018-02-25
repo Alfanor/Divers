298b3ca0e456d36cf9fb831f85584747ae653e01
Alfanor <alfanor@hotmail.fr>
2018-02-25 14:42:39 +0100

    Ajout du test avec Memcached, pas top en fait, c'est pour du cluster logiquement.

--- cache_test.php
+++ cache_test.php
@@ -9,12 +9,13 @@ ini_set("display_errors", 1);
  *      - SHM
  *
  *  BE CAREFUL : 
- *      - You need to install php-acpu and php-acpu-bc (backward-compatibility)
- *      - You need to uncomment these lines in your php.ini file :
+ *      - You need to install php-acpu and php-acpu-bc (backward-compatibility) for ACP
+ *      - You need to uncomment these lines in your php.ini file for SHM :
  *          extension=shmop.so
  *          extension=sysvmsg.so
  *          extension=sysvsem.so
  *          extension=sysvshm.so
+ *      - You need memcached and php-memcached for Memcached
  */
 require_once('Data.php');
 require_once('generate_database.php');
@@ -37,6 +38,12 @@ $len = strlen($serialized);
 $shm_id = shmop_open(0, "c", 0644, $len);
 shmop_write($shm_id, $serialized, 0);
 
+// Store them with Memcached
+$mem = new Memcached();
+$mem->addServer("127.0.0.1", 11211);
+
+$mem->add('data', $data);
+
 // Get Data object from APC
 $start_apc = microtime(true);
 
@@ -53,6 +60,13 @@ shmop_close($shm_id);
 
 $end_shm = microtime(true);
 
+// Get Data object from Memcached
+$start_memcached = microtime(true);
+
+$mem->get('data');
+
+$end_memcached = microtime(true);
+
 // Get Data object from Database
 $start_db = microtime(true);
 
@@ -63,5 +77,6 @@ $end_db = microtime(true);
 // Print the result
 echo 'Execution time for APC : ' . ($end_apc - $start_apc) . '<br />';
 echo 'Execution time for SHM : ' . ($end_shm - $start_shm) . '<br />';
+echo 'Execution time for Memcached : ' . ($end_memcached - $start_memcached) . '<br />';
 echo 'Execution time for Database : ' . ($end_db - $start_db) . '<br />';
 ?>
